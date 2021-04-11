57d0c0168738998db102d6fec8bfba920aa71328
SaphiraNgocThuy <saphira.ngoc.thuy@gmail.com>
2021-04-12 02:00:48 +0800

    fix create initial questions

--- server/dtb/DML/V1.0_create-initial-questions.sql
+++ server/dtb/DML/V1.0_create-initial-questions.sql
@@ -2,5 +2,5 @@ INSERT INTO questions (is_enable, question_type, question, placeholder, is_requi
 VALUE 
 (true, 'short_answer', 'What did you like most?', 'Tell us your experience', false),
 (true, 'short_answer', 'What did you like least?', 'Let us know how to improve', false),
-(true, 'email', 'Your email', 'Your email address', false);
-(true, 'linear_scale', 'How would you rate our UI design?', '', false)
+(true, 'email', 'Your email', 'Your email address', false),
+(true, 'linear_scale', 'How would you rate our UI design?', '', false);
