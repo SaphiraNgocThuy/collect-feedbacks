d5e9d8a670edafd116ac1b2bca9a85206740b6f6
SaphiraNgocThuy <nathalie.t@saltmine.com>
2021-04-11 18:21:18 +0800

    update data

--- server/dtb/DDL_01_create_feedbacks_table.sql
+++ server/dtb/DDL_01_create_feedbacks_table.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE feedbacks;
 USE feedbacks;
 CREATE TABLE feedbacks (id INTEGER PRIMARY KEY AUTO_INCREMENT, rating INT4);
-CREATE TABLE questions (id INTEGER PRIMARY KEY AUTO_INCREMENT, is_enable BOOLEAN default false, question_type ENUM('short_answer', 'email', 'linear_scale') NOT NULL, question VARCHAR(512));
+CREATE TABLE questions (id INTEGER PRIMARY KEY AUTO_INCREMENT, is_enable BOOLEAN default false, question_type ENUM('short_answer', 'email', 'linear_scale') NOT NULL, question VARCHAR(512), placeholder VARCHAR(512));
 CREATE TABLE feedback_details (feedback_id INTEGER REFERENCES feedbacks(feedback_id), question_id INTEGER REFERENCES questions(question_id), feedback VARCHAR(512), PRIMARY KEY (feedback_id, question_id));
--- server/index.js
+++ server/index.js
@@ -5,55 +5,55 @@ require('dotenv').config();
 
 // MYSQL connection
 function makeDb( config ) {
-    const connection = mysql.createConnection( config );
-    return {
-      query( sql, args ) {
-        return util.promisify( connection.query )
-          .call( connection, sql, args );
-      },
-      close() {
-        return util.promisify( connection.end ).call( connection );
-      }
-    };
+  const connection = mysql.createConnection( config );
+  return {
+    query( sql, args ) {
+      return util.promisify( connection.query )
+        .call( connection, sql, args );
+    },
+    close() {
+      return util.promisify( connection.end ).call( connection );
+    }
+  };
 }
 
 const dbConfig = {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    database: 'feedbacks'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  database: 'feedbacks'
 }
 const db = makeDb(dbConfig);
 
 // EXPRESS
 const app = express();
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', '*');
-    res.header('Access-Control-Allow-Methods', 'POST, PUT, GET, OPTIONS')
-    req.header('content-type', 'application/json');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', 'POST, PUT, GET, OPTIONS')
+  req.header('content-type', 'application/json');
 
-    if ('OPTIONS' === req.method) {
-      res.sendStatus(200);
-    } else {
-      next();
-    }
+  if ('OPTIONS' === req.method) {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
 });
 app.use(express.json());
 
 const port = process.env.BACKEND_PORT || 3001;
 
 app.listen(port, () => {
-    console.log(`Feedback Collection Widget listening at http://localhost:${port}`);
+  console.log(`Feedback Collection Widget listening at http://localhost:${port}`);
 })
 
 
 // API
 app.get('/ratings/', async (req, res) => {
-    const sqlQuery = 'SELECT rating FROM feedbacks';
-    const result = await db.query(sqlQuery);
-    res.send(result);
+  const sqlQuery = 'SELECT rating FROM feedbacks';
+  const result = await db.query(sqlQuery);
+  res.send(result);
 })
 
 
@@ -66,18 +66,21 @@ app.post('/ratings/', async (req, res) => {
 })
 
 app.get('/responses/', async (req, res) => {
-    const sqlQuery = 'SELECT like_most, like_least, email FROM feedbacks';
-    const result = await db.query(sqlQuery);
-    res.send(result);
+  const sqlQuery = 'SELECT f.id, fd.feedback, q.question FROM feedbacks f INNER JOIN feedback_details fd ON f.id=fd.feedback_id INNER JOIN questions q ON fd.question_id = questions.id';
+  const result = await db.query(sqlQuery);
+  res.send(result);
 })
 
 app.put('/responses/:id', async (req, res) => {
-    const response_id = req.params.id;
-    const {like_most, like_least, email} = req.body;
-    const sqlQuery = `UPDATE feedbacks SET like_most=${like_most? `'${like_most}'` : null}, like_least=${like_least? `'${like_least}'` : null}, email=${email? `'${email}'` : null} WHERE id = ${response_id}`;
-    const result = await db.query(sqlQuery);
-    res.status(200);
-    res.send({message: "Updated successfully."});
+  const response_id = req.params.id;
+  const {answers} = req.body;
+  const queries = answers.map(answer => {
+    const sqlQuery = `INSERT feedback_details SET feedback_id=${response_id}, question_id=${answer.question_id}, feedback=${answerfeedback}`;
+    return await db.query(sqlQuery);
+  });
+  Promise.all(queries);
+  res.status(200);
+  res.send({message: "Updated successfully."});
 })
 
 app.get('/questions/', async (req, res) => {
